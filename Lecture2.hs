36c70b055b3d0f1c73c7fd37fa2648baab86b4fa
Forest Tong <ftong@mit.edu>
2015-11-20 23:29:52 -0500

    Lecture 2

--- Lecture2.hs
+++ Lecture2.hs
@@ -1,3 +1,12 @@
+-- You'll notice that here we're using Empty instead of [] and Cons instead of :.
+-- This is simply because [] and : are reserved and we cannot redefine them.
+data IntList = Empty | Cons Int IntList
+
+data IntPair = IntPair Int Int
+
+sumPair :: IntPair -> Int
+sumPair (IntPair x y) = x + y
+
 myMap :: (a -> b) -> [a] -> [b]
 myMap f [] = []
 myMap f (x:xs) = (f x):(myMap f xs)
