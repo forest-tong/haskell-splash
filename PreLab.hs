b9205ab957773fed8769da83e67c479e89ecd8b9
Forest Tong <ftong@mit.edu>
2015-11-21 00:54:26 -0500

    a few exercises in prelab

---
+++ PreLab.hs
@@ -0,0 +1,19 @@
+-- Exercise 1
+-- Get the second element of a list.
+-- (Look up head and last.)
+snd :: [a] -> a
+
+-- Exercise 2
+-- Get the list [1, 2, ..., n].
+-- (Look up take.)
+firstN :: Int -> [Int]
+
+-- Exercise 3
+-- Calculate a^b.
+-- (Look up repeat.)
+power :: Int -> Int -> Int
+
+-- Exercise 4
+-- Calculate 1 + x + x^2 + x^3 + ... + x^n.
+-- (Look up iterate.)
+geometricSeries :: Int -> Int -> Int
