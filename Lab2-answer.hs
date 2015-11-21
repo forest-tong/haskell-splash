4a7e1835a254979224e9f5997b61357783549efd
Forest Tong <ftong@mit.edu>
2015-11-21 00:39:24 -0500

    skeleton for Lab 2 answer

--- Lab2-answer.hs
+++ Lab2-answer.hs
@@ -1,6 +1,34 @@
--- implement elem using fold 
-elem' :: (Eq a) => a -> [a] -> Bool  
-elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+-- Problem 1
+-- Reverse a list.
+reverse' :: [a] -> [a]
+reverse' list = foldl (flip (:)) [] list
 
-sum' :: (Num a) => [a] -> a  
-sum' xs = foldl (\acc x -> acc + x) 0 xs  
+-- Problem 2
+-- Implement map using fold.
+foldMap :: (a -> b) -> [a] -> [b]
+foldMap f list = foldl (\acc x -> acc ++ [f x]) [] list
+
+-- Problem 3
+-- Implement elem using fold.
+-- (Look up elem if you don't know what it does!)
+elem' :: (Eq a) => a -> [a] -> Bool  
+elem' y ys = foldl (\acc x -> acc || (x == y)) False ys
+
+-- The built-in algebraic data type "Maybe" represents values that might exist.
+-- Maybe is defined as follows:
+-- data Maybe a = Nothing | Just a
+
+-- Maybe is frequently useful in real-world scenarios to represent failed
+-- computations, instead of something like null in Java or None in Python.
+-- For example, Maybe can be used to represent the result of division:
+divide :: Float -> Float -> Maybe Float
+divide x 0 = Nothing
+divide x y = Just (x/y)
+
+-- Problem 4
+-- Define a data type called PersonName that consists of firstName,
+-- middleName, and lastName, where any of the names might not exist.
+
+-- Problem 5
+-- nameString is the concatenation of all the existing names of a PersonName,
+-- or Nothing if none of the names exist.
